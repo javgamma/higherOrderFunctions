ae21e2a037f26adb56981d26e3c574baf61108b8
Javmir Gamma <javmir.hernandez@students.gammatech.schoolgit config --global user.email javmir.hernandez@students.gammatech.school>
2024-06-10 21:28:42 +0200

    más ejercicios

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-// const numbers = [1, 2, 3, 4, 5];
-// numbers.forEach((num) => console.log(num));
+
 
 const countries = ["US", "Sweden", "Denmark", "Norway", "IceLand"];
 
 countries.forEach(country => console.log(country));
+
+
+const products = [
+    { product: "banana", price: 3 },
+    { product: "mango", price: 6 },
+    { product: "potato", price: " " },
+    { product: "avocado", price: 8 },
+    { product: "coffee", price: 10 },
+    { product: "tea", price: "" },
+  ];
 
 
 const names = ["Joseph", "Mathias", "Elias", "Brook"];
@@ -16,8 +25,6 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numbers.forEach(number => console.log(number));
 
 // MAP:
-
-
 
 const capitalLetters = countries.map((country) => country.toUpperCase());
  console.log(capitalLetters);
@@ -34,4 +41,24 @@ const capitalLetters = countries.map((country) => country.toUpperCase());
 
  //Utilice map para asignar el array de productos a sus correspondientes precios.
 
- 
+
+
+
+// const sixChartCountries = countries.filter((country)=>{
+//     return country.length === 6;
+
+// })
+// console.log(sixChartCountries)
+
+//12.
+
+const sixCharCountries = countries.filter((country) => {
+    return country.length === 6
+})
+console.log(sixCharCountries)
+
+//13
+const sixCharCountriesOrMore = countries.filter((country)=> {
+    return country.length >= 6;
+})
+console.log(sixCharCountriesOrMore);
